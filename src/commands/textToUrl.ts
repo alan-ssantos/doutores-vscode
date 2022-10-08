3eeec25ee7a331f531d336e6a2aef4ade875920b
Alan Santos <alansousa@outlook.com>
2022-10-08 20:31:54 -0300

    feature: add configurations to textToUrl command

--- src/commands/textToUrl.ts
+++ src/commands/textToUrl.ts
@@ -1,13 +1,14 @@
 import * as vscode from "vscode";
 import * as removeAccents from "remove-accents";
 
-const PREPOSITIONS_REGEX =
-  /(\s)a(\s)|(\s)ante(\s)|(\s)até(\s)|(\s)após(\s)|(\s)de(\s)|(\s)desde(\s)|(\s)em(\s)|(\s)entre(\s)|(\s)com(\s)|(\s)para(\s)|(\s)por(\s)|(\s)perante(\s)|(\s)sem(\s)|(\s)sob(\s)|(\s)sobre(\s)|(\s)na(\s)|(\s)no(\s)|(\s)e(\s)|(\s)do(\s)|(\s)da(\s)|(\s)de(\s)/g;
-
 async function textToUrl() {
+  const config = vscode.workspace.getConfiguration("doutores.textToUrl");
   const editor = vscode.window.activeTextEditor;
 
   if (editor) {
+    const prepositions: string[] = config.get("prepositions") || [];
+    const prepositionsRegex: RegExp = new RegExp(prepositions.map((p) => `(\\s)${p}(\\s)`).join("|"), "g");
+
     editor.edit((editBuilder) => {
       editor.selections.forEach((selection) => {
         const word = editor.document.getText(selection);
@@ -17,11 +18,14 @@ async function textToUrl() {
         slug = slug
           .normalize("NFD")
           .toLowerCase()
-          .replace(PREPOSITIONS_REGEX, " ")
-          .replace(/[!%.'$()*+/;=?\\,/:#@"\\[\]_“”÷°©®℗™ª]/gi, " ")
-          .replace(/<[^>]*>/gi, " ")
-          .trim()
-          .replace(/(\s+)/g, "-");
+          .replace(prepositionsRegex, " ")
+          .replace(/[!%.'$()*+;=?\\,:#@"\\[\]_“”÷°©®℗™ª]/g, " ");
+
+        if (config.get("removeHtmlTags")) {
+          slug = slug.replace(/<[^>]*>/gi, " ");
+        }
+
+        slug = slug.trim().replace(/(\s+)/g, "-");
 
         editBuilder.replace(selection, slug);
       });

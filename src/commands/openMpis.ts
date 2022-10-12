b40c7a7e4dd94c7500407af083ef3b3f070cc077
Alan Santos <alansousa@outlook.com>
2022-10-12 18:13:45 -0300

    refactor(openMpis): change method to open files

--- src/commands/openMpis.ts
+++ src/commands/openMpis.ts
@@ -2,30 +2,26 @@ import * as vscode from "vscode";
 import { getVetKey, getPathnames } from "../utils/vetKeyHandler";
 
 async function openMpis() {
-  try {
-    if (!vscode.workspace.workspaceFolders) {
-      throw new Error("Pasta do projeto não encontrada.");
-    }
-    const rootUri = vscode.workspace.workspaceFolders[0].uri;
+	try {
+		if (!vscode.workspace.workspaceFolders) {
+			throw new Error("Pasta do projeto não encontrada.");
+		}
+		const rootUri = vscode.workspace.workspaceFolders[0].uri;
 
-    const { content } = await getVetKey();
-    const pathnames = getPathnames(content);
+		const { content } = await getVetKey();
+		const pathnames = getPathnames(content);
 
-    for (const pathname of pathnames) {
-      try {
-        const doc = await vscode.workspace.openTextDocument(`${rootUri.fsPath}/${pathname}.php`);
-        vscode.window.showTextDocument(doc, { preview: false });
-      } catch (error) {
-        vscode.window.showErrorMessage(`Não foi possível abrir o arquivo ${pathname}.php`);
-      }
-    }
-  } catch (e) {
-    if (typeof e === "string") {
-      vscode.window.showErrorMessage(e);
-    } else if (e instanceof Error) {
-      vscode.window.showErrorMessage(e.message);
-    }
-  }
+		for (let index = 0; index < pathnames.length; index++) {
+			const uri = vscode.Uri.file(`${rootUri.fsPath}/${pathnames[index]}.php`);
+			vscode.commands.executeCommand("vscode.open", uri, { preview: false, preserveFocus: false });
+		}
+	} catch (e) {
+		if (typeof e === "string") {
+			vscode.window.showErrorMessage(e);
+		} else if (e instanceof Error) {
+			vscode.window.showErrorMessage(e.message);
+		}
+	}
 }
 
 export default openMpis;

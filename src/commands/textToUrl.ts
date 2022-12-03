ce6ccc8825451e984b75360ce0656cd3e33fe868
Alan Santos <alansousa@outlook.com>
2022-12-03 15:18:51 -0300

    fix(textToUrl): remove extra dash and fowardslash

--- src/commands/textToUrl.ts
+++ src/commands/textToUrl.ts
@@ -2,35 +2,34 @@ import * as vscode from "vscode";
 import * as removeAccents from "remove-accents";
 
 async function textToUrl() {
-  const config = vscode.workspace.getConfiguration("doutores.textToUrl");
-  const editor = vscode.window.activeTextEditor;
+	const config = vscode.workspace.getConfiguration("doutores.textToUrl");
+	const editor = vscode.window.activeTextEditor;
 
-  if (editor) {
-    const prepositions: string[] = config.get("prepositions") || [];
-    const prepositionsRegex: RegExp = new RegExp(prepositions.map((p) => `(\\s)${p}(\\s)`).join("|"), "g");
+	if (editor) {
+		const prepositions: string[] = config.get("prepositions") || [];
+		const prepositionsRegex: RegExp = new RegExp(prepositions.map((p) => `(\\s)${p}(\\s)`).join("|"), "g");
 
-    editor.edit((editBuilder) => {
-      editor.selections.forEach((selection) => {
-        const word = editor.document.getText(selection);
+		editor.edit((editBuilder) => {
+			editor.selections.forEach((selection) => {
+				const word = editor.document.getText(selection);
 
-        let slug: string;
-        slug = removeAccents(word);
-        slug = slug
-          .normalize("NFD")
-          .toLowerCase()
-          .replace(prepositionsRegex, " ")
-          .replace(/[!%.'$()*+;=?\\,:#@"\\[\]_“”÷°©®℗™ª]/g, " ");
+				let slug: string;
+				slug = removeAccents(word);
+				slug = slug
+					.normalize("NFD")
+					.toLowerCase()
+					.replace(prepositionsRegex, " ")
+					.replace(/[!%.'$()*+;=?\\,:#@"\\[\]_\/“”÷°©®℗™ª]/g, " ");
 
-        if (config.get("removeHtmlTags")) {
-          slug = slug.replace(/<[^>]*>/gi, " ");
-        }
+				if (config.get("removeHtmlTags")) {
+					slug = slug.replace(/<[^>]*>/g, " ");
+				}
+				slug = slug.trim().replace(/(\s+)/g, "-").replace(/\-+/g, "-");
 
-        slug = slug.trim().replace(/(\s+)/g, "-");
-
-        editBuilder.replace(selection, slug);
-      });
-    });
-  }
+				editBuilder.replace(selection, slug);
+			});
+		});
+	}
 }
 
 export default textToUrl;

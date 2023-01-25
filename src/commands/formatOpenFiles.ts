bad12465b741aa03858d1faac4c7afe297b800b4
Alan Santos <alansousa@outlook.com>
2023-01-25 15:28:16 -0300

    feat(formatOpenFiles): add progress notification

--- src/commands/formatOpenFiles.ts
+++ src/commands/formatOpenFiles.ts
@@ -6,18 +6,40 @@ async function formatOpenFiles() {
 
 	if (editor) {
 		const documentos = vscode.workspace.textDocuments;
-
 		const langs = ["php", "json", "js", "ts", "html", "css"];
-		for (let index = 0; index < documentos.length; index++) {
-			if (langs.includes(documentos[index].languageId)) {
-				await vscode.commands.executeCommand("editor.action.formatDocument");
-				await vscode.window.showTextDocument(documentos[index], { preview: false, preserveFocus: false });
-			}
-		}
 
-		if (config.get("autoSave") === true) {
-			await vscode.commands.executeCommand("workbench.action.files.saveAll");
-		}
+		vscode.window.withProgress(
+			{
+				location: vscode.ProgressLocation.Notification,
+				cancellable: true,
+				title: "Formatando arquivos abertos...",
+			},
+			async (progress, token) => {
+				let cancel = false;
+				token.onCancellationRequested(() => {
+					cancel = true;
+				});
+
+				progress.report({ increment: 0 });
+
+				for (let index = 0; index < documentos.length; index++) {
+					if (cancel) {
+						break;
+					}
+
+					if (langs.includes(documentos[index].languageId)) {
+						await vscode.commands.executeCommand("editor.action.formatDocument");
+						vscode.window.showTextDocument(documentos[index], { preview: false, preserveFocus: false });
+					}
+
+					progress.report({ increment: 100 / documentos.length, message: `${index} / ${documentos.length}` });
+				}
+
+				if (config.get("autoSave") === true) {
+					await vscode.commands.executeCommand("workbench.action.files.saveAll");
+				}
+			}
+		);
 	}
 }
 

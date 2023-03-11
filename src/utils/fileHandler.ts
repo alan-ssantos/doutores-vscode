8e9a5295f9793d6bbf9508f67ad684b6da3290b7
Alan Santos <alansousa@outlook.com>
2023-03-11 16:16:17 -0300

    fix(fileHandler): change getPathnames function to return pathnames with numbers

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,29 +1,30 @@
 import * as vscode from "vscode";
 
 interface VetKeyInterface {
-  fileUri: vscode.Uri;
-  content: string;
+	fileUri: vscode.Uri;
+	content: string;
 }
 
 export async function getFile(rootUri: vscode.Uri): Promise<VetKeyInterface> {
-	const filePath: string = vscode.workspace.getConfiguration("doutores.openFiles").get("filePath") || '';
+	const filePath: string = vscode.workspace.getConfiguration("doutores.openFiles").get("filePath") || "";
 	const fileUri = vscode.Uri.file(`${rootUri.fsPath}/${filePath}`);
 
 	let content = (await vscode.workspace.fs.readFile(fileUri)).toString();
 
 	return {
 		fileUri,
-		content
+		content,
 	};
 }
 
 export function getPathnames(vetKey: string): string[] {
-	const urls = vetKey.match(/url"\s=>\s"((\w+-)+\w+)/g);
+	const urls = [...vetKey.matchAll(/"url"\s*=>\s*"([a-z0-9-]+)"/g)];
+
 	if (!urls) {
 		throw new Error("Nenhum caminho foi encontrado no arquivo encontrada na vetKey.");
 	}
 
-	const pathnames = urls?.map((url) => url.replace(/url"\s=>\s"/g, ""));
+	const pathnames = urls.map(([match, slug]) => slug);
 
 	return pathnames;
 }

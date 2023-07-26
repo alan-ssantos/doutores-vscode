909dbbf8a609f67a0395a78bc95c9c731eaecd1c
Alan Santos <alansousa@outlook.com>
2023-07-26 00:02:19 -0300

    refactor(generateColors): change method to generate colors and the output names of variables

--- src/commands/generateColors.ts
+++ src/commands/generateColors.ts
@@ -1,59 +1,48 @@
 import * as vscode from "vscode";
 import { TinyColor } from "@ctrl/tinycolor";
 
-async function textToUrl() {
-	// const config = vscode.workspace.getConfiguration("drs.textToUrl");
+async function generateColors() {
 	const editor = vscode.window.activeTextEditor;
 
 	if (editor) {
-		editor.edit((editBuilder) => {
-			editor.selections.forEach((selection) => {
-				const textSelected = editor.document.getText(selection);
+		try {
+			editor.edit((editBuilder) => {
+				editor.selections.forEach((selection) => {
+					const textSelected = editor.document.getText(selection);
 
-				let colorName: string = "";
-				let color: TinyColor;
-        let selectedLine = false;
-				const match = textSelected.match(/--(\w*\-*)*:\s*#([A-Fa-f0-9]{3,6});/g);
+					let colorName: string = "";
+					let color: TinyColor;
+					const match = textSelected.match(/--(\w*\-*)*:\s*#([A-Fa-f0-9]{3,6});/g);
 
-				if (match && match.length >= 1) {
-          selectedLine = true;
-					const nameMatch = textSelected.match(/(\-+(\w*\-?)*)/g);
-					const colorMatch = textSelected.match(/#([A-Fa-f0-9]{3,6})/g);
-
-					if (!colorMatch || !nameMatch) {
-						console.log("Erro ao localizar a cor na linha selecionada.");
-						return;
+					if (match && match.length >= 1) {
+						colorName = textSelected.split(": ")[0];
+						color = new TinyColor(textSelected.split(": ")[1].replace(";", ""));
 					} else {
-						colorName = nameMatch[0];
-						color = new TinyColor(colorMatch[0]);
+						color = new TinyColor(textSelected);
 					}
-				} else {
-					color = new TinyColor(textSelected);
-				}
 
-				if (!color.isValid) {
-					console.log("A cor selecionada não é válida");
-					return;
-				}
+					if (!color.isValid) {
+						throw new Error("A cor selecionada não é válida");
+					}
 
-				let colors: TinyColor[] = [];
-				colors.push(color.tint(40));
-				colors.push(color.shade(40));
+					const lighterColor = color.mix("#FFF", 35);
+					const darkerColor = color.mix("#000", 35);
 
-				let result = "";
-				for (let index = 0; index < colors.length; index++) {
-          let name = colorName ? `${colorName}-${index}` : `--color-name-${index}`;
-          if (selectedLine) {
-            result = result.concat(`\n\t${name}: #${colors[index].toHex()};`);
-          } else {
-            result = result.concat(`;\n\t${name}: #${colors[index].toHex()}`);
-          }
-				}
+					let result = `${textSelected}${colorName ? `\n\t${colorName}-light` : ";\n\t--color-light"}: #${lighterColor.toHex()};\n\t${
+						colorName ? `${colorName}-dark: #${darkerColor.toHex()};` : `--color-dark: #${darkerColor.toHex()}`
+					}`;
 
-				editBuilder.replace(selection, `${textSelected}${result}`);
+					editBuilder.replace(selection, `${result}`);
+				});
 			});
-		});
+		} catch (e) {
+			if (typeof e === "string") {
+				vscode.window.showErrorMessage(e);
+			} else if (e instanceof Error) {
+				vscode.window.showErrorMessage(e.message);
+			}
+		}
 	}
 }
 
-export default textToUrl;
+export default generateColors;

715e34e65a4cfbe3a054abe211251be1fb769bee
Alan Santos <alansousa@outlook.com>
2023-12-03 14:46:31 -0300

    refactor(turnVet): update command to show input dialog and improve code

--- src/commands/turnVet.ts
+++ src/commands/turnVet.ts
@@ -3,26 +3,36 @@ import makeSlug from "../utils/makeSlug";
 
 async function turnVet() {
 	try {
+		const config = vscode.workspace.getConfiguration("drs.textToUrl");
+		const removeHtmlTags: boolean = config.get("removeHtmlTags") ?? false;
+		const prepositions: string[] = config.get("prepositions") ?? [];
+
 		const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			editor.edit((builder) => {
-				editor.selections.forEach((selection, index) => {
-					if (!selection.isEmpty) {
-						let text = editor.document.getText(selection);
-
-						let vetItems = text.split("\n");
-						let vetContent = vetItems
-							.map((i) => `\n\t["url" => "${makeSlug(i)}", "title" => "${i}", "cover" => null, "sub-menu" => null],`)
-							.join("");
-
-						builder.replace(selection, `<?php $vetName${editor.selections.length > 1 ? index : ""} = array(${vetContent}\n); ?>`);
-					} else {
-						throw new Error("Selecione o conteúdo que deve ser convertidos em vet");
-					}
-				});
-			});
+		if (!editor) {
+			vscode.window.showErrorMessage("Nenhum editor de texto ativo.");
+			return;
 		}
+
+		const vetName = await vscode.window.showInputBox({ title: "Digite o nome do vetor:", placeHolder: "Ex.: vetServicos" });
+
+		if (!vetName) {
+			vscode.window.showErrorMessage("É preciso definir um nome para o vetor");
+			return;
+		}
+
+		editor.edit((builder) => {
+			editor.selections.forEach((selection, index) => {
+				if (!selection.isEmpty) {
+					let text = editor.document.getText(selection);
+					let vetContent = createVetContent(text, removeHtmlTags, prepositions);
+
+					builder.replace(selection, `<?php $${vetName}${editor.selections.length > 1 ? index : ""} = array(${vetContent}\n); ?>`);
+				} else {
+					vscode.window.showErrorMessage("Selecione o conteúdo que deve ser convertido em vetor");
+				}
+			});
+		});
 	} catch (e) {
 		if (typeof e === "string") {
 			vscode.window.showErrorMessage(e);
@@ -30,6 +40,16 @@ async function turnVet() {
 			vscode.window.showErrorMessage(e.message);
 		}
 	}
+}
+
+function createVetContent(text: string, removeHtmlTags: boolean, prepositions: string[]): string {
+	let vetItems = text.split("\n");
+	return vetItems
+		.map((i) => {
+			let item = i.trim();
+			return `\n\t["url" => "${makeSlug(item, removeHtmlTags, prepositions)}", "title" => "${item}", "cover" => null, "sub-menu" => null],`;
+		})
+		.join("");
 }
 
 export default turnVet;

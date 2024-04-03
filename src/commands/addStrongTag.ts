9871fea6c87f076067d878a8614adda352343446
Alan Santos <alansousa@outlook.com>
2024-04-03 00:10:49 -0300

    add link to files on message errors of `replaceDescription` and `addStrongTag`

--- src/commands/addStrongTag.ts
+++ src/commands/addStrongTag.ts
@@ -4,13 +4,12 @@ import { getPageTitle } from "../utils/mpiHelper";
 async function addStrongTag() {
 	const config = vscode.workspace.getConfiguration("drs.addStrongTag");
 
-	try {
-		const editor = vscode.window.activeTextEditor;
+	const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
-			const document = editor.document;
-			const documentText = document.getText();
-
+	if (editor) {
+		const document = editor.document;
+		const documentText = document.getText();
+		try {
 			const pageTitle = getPageTitle(documentText);
 			const paragraphs = documentText.match(/<p[^>]*>(.|\n)*?<\/p>/g);
 
@@ -33,12 +32,12 @@ async function addStrongTag() {
 			if (config.get("autoSave") === true) {
 				await vscode.commands.executeCommand("workbench.action.files.save");
 			}
-		}
-	} catch (e) {
-		if (typeof e === "string") {
-			vscode.window.showErrorMessage(e);
-		} else if (e instanceof Error) {
-			vscode.window.showErrorMessage(e.message);
+		} catch (e) {
+			if (typeof e === "string") {
+				vscode.window.showErrorMessage(e);
+			} else if (e instanceof Error) {
+				vscode.window.showErrorMessage(`${e.message} [${document.fileName.replace(/.*(?<=\\)/g, "")}](${document.uri.toString()})`);
+			}
 		}
 	}
 }
